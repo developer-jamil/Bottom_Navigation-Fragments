93030914c38be2ebb774f68a1d201a2b56b6468f
Developer_Jamil <developerjamil@gmail.com>
2023-03-13 12:29:57 +0600

    Name: Bottom Navigation BackPress update Version : 1-1.0

--- app/src/main/java/com/jamillabltd/bottomnavigationone/BottomFragments/HomeFragment.java
+++ app/src/main/java/com/jamillabltd/bottomnavigationone/BottomFragments/HomeFragment.java
@@ -18,7 +18,4 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
-
-
 }
