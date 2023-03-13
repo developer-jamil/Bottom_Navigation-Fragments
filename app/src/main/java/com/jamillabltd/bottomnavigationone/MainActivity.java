051df364dd638d8fa4dfab08eb25ad0dadb0938f
Developer_Jamil <developerjamil@gmail.com>
2023-03-14 02:57:25 +0600

    Name: Bottom Navigation BackPress update Version : 1-1.0

--- app/src/main/java/com/jamillabltd/bottomnavigationone/MainActivity.java
+++ app/src/main/java/com/jamillabltd/bottomnavigationone/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jamillabltd.bottomnavigationone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.jamillabltd.bottomnavigationone.BottomFragments.CartFragment;
 import com.jamillabltd.bottomnavigationone.BottomFragments.HomeFragment;
 import com.jamillabltd.bottomnavigationone.BottomFragments.MessageFragment;
@@ -15,46 +18,41 @@ import com.jamillabltd.bottomnavigationone.BottomFragments.ProfileFragment;
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //bottom navigation
+        //initialize bottom navigation
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        //default fragment set - HomeFragment
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+
+
+        //bottom navigation setOnItemSelectedListener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.menu_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.menu_message:
+                    selectedFragment = new MessageFragment();
+                    break;
+                case R.id.menu_cart:
+                    selectedFragment = new CartFragment();
+                    break;
+                case R.id.menu_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
     }
-
-
-    //bottom navigation icon click
-    @SuppressLint("NonConstantResourceId")
-    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            item -> {
-                Fragment selectedFragment = null;
-
-                switch (item.getItemId()) {
-                    case R.id.menu_home:
-                        selectedFragment = new HomeFragment();
-                        break;
-                    case R.id.menu_message:
-                        selectedFragment = new MessageFragment();
-                        break;
-                    case R.id.menu_cart:
-                        selectedFragment = new CartFragment();
-                        break;
-                    case R.id.menu_profile:
-                        selectedFragment = new ProfileFragment();
-                        break;
-                }
-
-                assert selectedFragment != null;
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        selectedFragment).commit();
-
-                return true;
-            };
 
 
     @Override
